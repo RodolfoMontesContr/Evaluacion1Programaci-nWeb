91d35d7590ba273925097e65fc0faa39c4357728
Roldofo Montes <rod.montes@duocuc.cl>
2022-05-09 11:01:52 -0400

    Intento api clima

--- clima.js
+++ clima.js
@@ -1,14 +1,15 @@
-let lon
+/*let lon
 let lat
 window.addEventListener('load',()=> {
-    if(navigator.geolocation9){
+    if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(posicion =>{
             console.log(posicion.coords.latitude)
             lon = posicion.coords.longitude
             lat = posicion.coords.latitude
 
-            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e5c4b7a7ae70a6c41ca05733f3807ab7`
+            //const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e5c4b7a7ae70a6c41ca05733f3807ab7`
 
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=Madrid&lang=es&units=metric&appid=e5c4b7a7ae70a6c41ca05733f3807ab7`
             console.log(url)
 
             fetch(url)
@@ -23,4 +24,17 @@ window.addEventListener('load',()=> {
 }
 
 
-)
+)*/
+
+var getIP = 'http://ip-api.com/json/';
+var openWeatherMap = 'http://api.openweathermap.org/data/2.5/weather'
+$.getJSON(getIP).done(function(location) {
+    $.getJSON(openWeatherMap, {
+        lat: location.lat,
+        lon: location.lon,
+        units: 'metric',
+        appid: 'e5c4b7a7ae70a6c41ca05733f3807ab7'
+    }).done(function(weather) {
+        console.log(weather)
+    })
+})
