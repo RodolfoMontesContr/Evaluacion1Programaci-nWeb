3e25f6ef7bceb1849d0a2d9093cf5d487baed994
Roldofo Montes <rod.montes@duocuc.cl>
2022-05-09 11:35:33 -0400

    intentos api clima

--- clima.js
+++ clima.js
@@ -1,30 +1,32 @@
-/*let lon
-let lat
 window.addEventListener('load',()=> {
+let lon
+let lat
+let temperaturaValor = document.getElementById('temperatura')
+let temperaturadescrip = document.getElementById('temperatura-descripcion')
+let ciudad = document.getElementById('ciudadactual')
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(posicion =>{
-            console.log(posicion.coords.latitude)
             lon = posicion.coords.longitude
             lat = posicion.coords.latitude
 
-            //const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e5c4b7a7ae70a6c41ca05733f3807ab7`
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e5c4b7a7ae70a6c41ca05733f3807ab7`
 
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=Madrid&lang=es&units=metric&appid=e5c4b7a7ae70a6c41ca05733f3807ab7`
+
             console.log(url)
+            console.log("hola")
 
-            fetch(url)
-                .then (response => { return response.json()})
-                .then (data =>{
-                    console.log(data.main.temp)
-                }
-                    
-                    )
         })
     }
 }
 
 
-)*/
+)
+
+/*window.addEventListener('load',()=>{
+    if (navigator)
+}
+
+)
 
 var getIP = 'http://ip-api.com/json/';
 var openWeatherMap = 'http://api.openweathermap.org/data/2.5/weather'
@@ -36,5 +38,14 @@ $.getJSON(getIP).done(function(location) {
         appid: 'e5c4b7a7ae70a6c41ca05733f3807ab7'
     }).done(function(weather) {
         console.log(weather)
+        console.log(location)
     })
 })
+window.addEventListener('load',()=>{
+var openWeatherMap = 'http://api.openweathermap.org/data/2.5/weather'
+if (window.navigator && window.navigator.geolocation) {
+    window.navigator.geolocation.getCurrentPosition(function(position) {
+        console.log(positon)
+        })
+    }
+})*/
