ddf1410412c50ce29cd1ad468577aa31d7cf9499
eliasramirezsilva <el.ramirezs@duocuc.cl>
2022-05-18 10:18:50 -0400

    actualizacion appi perros en json

--- perro.js
+++ perro.js
@@ -2,14 +2,14 @@
 mostrar.addEventListener('click', function () { 
 
   
-    fetch(`https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1`)
+    fetch(`https://www.dogfactsapi.ducnguyen.dev/api/v1/facts/?number=1`)
       .then(respuesta => respuesta.json())
       .then(datos =>{
 
         console.log(datos)
        
         var out = "===========Curiosidad========== "
-        out = out + JSON.stringify(datos[0].fact)
+        out = out + JSON.stringify(datos.facts[0])
         document.getElementById("curiosidad").innerText = out;
       })
   }
